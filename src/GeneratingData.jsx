1acc105be912da75286e2fdb6cd498cd1c4260aa
Rakesh Patel <rakesh246808@gmail.com>
2024-10-02 20:08:42 +0530

    succesfully fatched data from api

--- src/GeneratingData.jsx
+++ src/GeneratingData.jsx
@@ -1,21 +1,43 @@
-import { useState } from "react"
+import { useState } from "react";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_REACT_APP_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 function GeneratingData()
 {
+    const [prompt,setPrompt] = useState(" ");
     const [text,generatedText] = useState(" ");
 
-    function data()
+    let allContents = []
+
+    function data(e)
     {
-        
+        setPrompt(e.target.value);
     }
 
+    async function generateContent()
+    {
+        
+        const result = await model.generateContent(prompt);
+        generatedText(result.response.text());
+    }
+    
+    allContents = text.split(".");
+
     return(
+        <>
+        <img className="bg" src="/src/assets/bg.jpg" alt="" />
         <div className="container">
-            <input type="text" onChange={data} />
-            <ul>
-                
+            <input type="text" value={prompt} onChange={data} />
+            <ul className="generatexText-container">
+                {allContents.map((data,index) => {
+                    return <li className="generatexText" key={index}>{data}</li>
+                })}
             </ul>
+            <button id="submitBtn" onClick={generateContent}>SUBMIT</button>
         </div>
+        </>
     )
 }
 
