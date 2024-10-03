17e73f50dc732586d51a39f4f45730d7f7ff039c
Rakesh Patel <rakesh246808@gmail.com>
2024-10-03 12:24:11 +0530

    fixed some bugs

--- src/GeneratingData.jsx
+++ src/GeneratingData.jsx
@@ -9,6 +9,8 @@ function GeneratingData()
     const [prompt,setPrompt] = useState(" ");
     const [text,generatedText] = useState(" ");
 
+    const[containerId,setContainerId] = useState("hideContainer");
+
     let allContents = []
 
     function data(e)
@@ -18,22 +20,24 @@ function GeneratingData()
 
     async function generateContent()
     {
-        
         const result = await model.generateContent(prompt);
         generatedText(result.response.text());
+        setContainerId("displayContainer")
     }
     
     allContents = text.split(".");
+    allContents.length = allContents.length - 1;
 
     return(
         <>
         <img className="bg" src="/src/assets/bg.jpg" alt="" />
         <div className="container">
-            <input type="text" value={prompt} onChange={data} />
-            <ul className="generatexText-container">
-                {allContents.map((data,index) => {
+            <input type="text" placeholder="write what you want to search... " value={prompt} onChange={data} />
+            <ul className="generatexText-container" id={containerId}>
+            {(allContents.length > 1) ? allContents.map((data,index) => {
                     return <li className="generatexText" key={index}>{data}</li>
-                })}
+                }) : null}
+               
             </ul>
             <button id="submitBtn" onClick={generateContent}>SUBMIT</button>
         </div>
